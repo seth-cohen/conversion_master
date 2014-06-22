92ed7ab7c9e0b428d7e7edae91dbdb9a39e64546
Seth Cohen <seth@Seths-MacBook-Pro.local>
2014-06-22 16:57:25 -0400

    Initial Commit

---
+++ Conversion Master/FTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FTAppDelegate.h
+//  Conversion Master
+//
+//  Created by Seth Cohen on 6/22/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Conversion Master/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Conversion Master
+//
+//  Created by Seth Cohen on 6/22/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FTAppDelegate class]));
+    }
+}
